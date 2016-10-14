790190076c15e77cd9d831ec13d1ef33d44689fc
DavidZhiXing <wow_only@163.com>
2016-10-14 18:07:05 +0800

    ..

--- Encoding.rb
+++
@@ -1,9 +0,0 @@
-$KCODE = 'u'
-require 'jcode'
-#!/usr/bin/ruby -Ku -rjcode
-string = "\xef\xbc\xa1"+"\xef\xbc\xa2"+
-        "\xef\xbc\xa3"+"\xef\xbc\xa4"+
-        "\xef\xbc\xa5"+"\xef\xbc\xa6"
-
-puts string.size
-puts string.jsize
